2f48054bbd90954847926c87e6f6f4de1283bee3
Shubham7488 <shubhamshaw930@gmail.com>
2026-01-22 21:00:02 +0530

    Change in next.config

--- makemytour/next.config.ts
+++ makemytour/next.config.ts
@@ -1,8 +1,20 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   reactStrictMode: true,
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
